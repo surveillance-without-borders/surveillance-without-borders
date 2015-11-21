25063a39884289abad22569bd32e51f409d04f89
Julia Whistle <julia.whistle@tutanota.io>
2015-11-21 05:13:26 +0100

    Implement basic parsing of CSVs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,26 @@
  * Controller of the cloakAndDaggerApp
  */
 angular.module('cloakAndDaggerApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.mapObject = {
-        scope: 'world',
-        options: {
-            height: 400,
-            legendHeight: 60 // optionally set the padding for the legend
-        },
-    }
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    console.log(d3);
+    $http.get('data/Politicians.csv').then(function(response) {
+        var country_mapping = {};
+        angular.forEach(d3.csv.parse(response.data), function(row) {
+            angular.forEach(row["Countries"].split(","), function(country) {
+                country = country.replace(/^\s+/g, '');
+                if (typeof country_mapping[country] == 'undefined') {
+                    country_mapping[country] = [];
+                }
+                country_mapping[country].push(row);
+            });
+        });
+        console.log(country_mapping);
+        $scope.mapObject = {
+            scope: 'world',
+            options: {
+                height: 400,
+                legendHeight: 60 // optionally set the padding for the legend
+            },
+        }
+    })
   }]);
