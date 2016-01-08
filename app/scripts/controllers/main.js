4a034dc2a91b720e408e93e483364a183b05adca
Julia Whistle <julia.whistle@tutanota.io>
2016-01-08 22:45:05 +0100

    Implement stable map
    * Cache responses

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the cloakAndDaggerApp
  */
 angular.module('cloakAndDaggerApp')
-  .controller('MainCtrl', ['$scope', '$rootScope', '$http',
-              function ($scope, $rootScope, $http) {
+  .controller('MainCtrl', ['$scope', '$rootScope', '$http', '$anchorScroll', '$location',
+              function ($scope, $rootScope, $http, $anchorScroll, $location) {
     var csvToCategory = {
             "Leaders": 'data/Politicians-processed.csv',
             "Corporate collaboration": 'data/CollaborationIAC-processed.csv',
@@ -18,60 +18,79 @@ angular.module('cloakAndDaggerApp')
             "Mass": 'data/PubMASS-processed.csv',
             "Targeted": 'data/PubTARGETED-processed.csv'
     },
-        countryMapping = {},
-        loadedCategories = [];
-    function loadMap(category) {
-        $http.get(csvToCategory[category]).then(function(response) {
-            var countryColors = {};
-            angular.forEach(d3.csv.parse(response.data), function(row) {
-                angular.forEach(row['Countries'].split(","), function(country) {
-                    country = country.replace(/^\s+/g, '');
-                    if (typeof countryMapping[country] === 'undefined') {
-                        countryMapping[country] = {}
-                    }
-                    if (typeof countryMapping[country][category] === 'undefined') {
-                        countryMapping[country][category] = [];
-                    }
-                    if (country.match(/^[A-Z]+$/)) {
-                        // XXX This is super ghetto
-                        if (loadedCategories.indexOf(category) == -1) {
-                            countryMapping[country][category].push(row);
+        countryMapping = {};
+
+    function loadCategoryData(category, cb) {
+        if (typeof countryMapping[category] !== "undefined") {
+            cb(countryMapping[category]);
+        } else {
+            countryMapping[category] = {};
+            $http.get(csvToCategory[category]).then(function(response) {
+                angular.forEach(d3.csv.parse(response.data), function(row) {
+                    angular.forEach(row['Countries'].split(","), function(country) {
+                        country = country.replace(/^\s+/g, '');
+                        if (typeof countryMapping[category][country] === 'undefined') {
+                            countryMapping[category][country] = [];
                         }
-                        countryColors[country] = {
-                            "fillKey": "EXISTS"
-                        };
-                    }
+                        if (country.match(/^[A-Z]+$/)) {
+                            countryMapping[category][country].push(row);
+                        }
+                    });
                 });
+                cb(countryMapping[category]);
             });
-            $scope.mapObject = {
-                scope: 'world',
-                options: {
-                    height: 300,
-                    legendHeight: 60
-                },
-                fills: {
-                    'EXISTS': '#CC4731',
-                    'defaultFill': '#DDDDDD'
-                },
-                data: countryColors
-            };
-            loadedCategories.push(category);
+        }
+    }
+
+    function loadCategory(category, cb) {
+        loadCategoryData(category, function(countryData) {
+            var countryColors = {};
+            if ($scope.selectedCountry !== undefined) {
+                countryColors[$scope.selectedCountry.id] = {
+                    'fillKey': 'SELECTED'
+                }
+            }
+            for (var alpha3 in countryData) {
+                if (!$scope.selectedCountry || alpha3 != $scope.selectedCountry.id) {
+                    countryColors[alpha3] = {
+                        'fillKey': 'EXISTS'
+                    }
+                }
+                $scope.mapObject = {
+                    scope: 'world',
+                    options: {
+                        height: 300,
+                        legendHeight: 60
+                    },
+                    fills: {
+                        'EXISTS': '#67C378',
+                        'SELECTED': '#704279',
+                        'defaultFill': '#DDDDDD'
+                    },
+                    data: countryColors
+                };
+            }
+            if ($scope.selectedCountry) {
+                $scope.countryData = countryData[$scope.selectedCountry.id];
+            }
+            cb();
         });
     }
 
-    $scope.infoBox = false;
-
     $scope.openPanel = function(category) {
         $scope.selectedCategory = category;
-        $scope.infoBox = false;
-        loadMap(category);
+        loadCategory(category);
     };
 
     $scope.updateActiveGeography = function(geo) {
-        $scope.selectedCountry = geo.properties.name;
-        $scope.infoBox = true;
-        $scope.geoData = countryMapping[geo.id][$scope.selectedCategory];
-        $scope.$apply();
+        $scope.selectedCountry = {
+            "name": geo.properties.name,
+            "id": geo.id
+        };
+        loadCategory($scope.selectedCategory, function(){
+            $scope.$apply();
+            $anchorScroll('details');
+        });
     };
     $scope.openPanel("Leaders");
   }]);
